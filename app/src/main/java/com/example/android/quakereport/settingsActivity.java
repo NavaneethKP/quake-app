20e27a2f4b5cb227e6d9fcd995df2f6cedae01cf
KP Navaneeth <kpnavaneeth12@gmail.com>
2018-01-02 15:23:18 +0530

    feat : added list preference to choose orderby option.

--- app/src/main/java/com/example/android/quakereport/settingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/settingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.SharedPreferences;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
@@ -20,18 +21,35 @@ public class settingsActivity extends AppCompatActivity {
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
+
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
 
+            Preference orderby = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderby);
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
 
-            preference.setSummary(newValue.toString());
+            String stringValue = newValue.toString();
+
+            if(preference instanceof ListPreference)
+            {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+
+                if(prefIndex >= 0)
+                {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            }
+
+            preference.setSummary(stringValue);
             return true;
         }
 
@@ -40,7 +58,7 @@ public class settingsActivity extends AppCompatActivity {
 
             preference.setOnPreferenceChangeListener(this);
             SharedPreferences preferences = preference.getSharedPreferences();
-            String preferenceString = preferences.getString(getString(R.string.settings_min_magnitude_key),"");
+            String preferenceString = preferences.getString(preference.getKey(), "");
             onPreferenceChange(preference, preferenceString);
 
         }
